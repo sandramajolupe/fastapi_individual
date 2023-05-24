7a2d8b54d384bfb48a546cbc63a781f5bd0a6813
Javier Sanchez <javier.esand@gmail.com>
2023-05-24 10:52:30 -0500

    create models genres and movie genres

---
+++ models/genres.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from config.database import Base
+
+
+class Genres(Base):
+
+    __tablename__ ="genres"
+
+    id = Column(Integer, primary_key=True)
+    gen_title = Column(String)

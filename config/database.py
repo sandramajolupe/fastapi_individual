b0de47e2b4df1f0690b58ccaf233073413b1d3dc
Javier Sanchez <javier.esand@gmail.com>
2023-05-24 08:43:20 -0500

    stabilice the project

--- config/database.py
+++ config/database.py
@@ -7,7 +7,6 @@ sqlite_file_name = "../database.sqlite"
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
 database_url = f"sqlite:///{os.path.join(base_dir, sqlite_file_name)}"
-
 engine = create_engine(database_url, echo=True)
 
 Session = sessionmaker(bind=engine)

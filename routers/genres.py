c739f60b37cdb3cd1e265e64008ab2b758e23607
Javier Sanchez <javier.esand@gmail.com>
2023-05-29 13:01:52 -0500

    genres get get or id and post

--- routers/genres.py
+++ routers/genres.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter,Path, Query
 from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+
+from service.genres import GenresService
+from schemas.genres import Genres 
+from config.database import Session
+
 
 
 genres_router = APIRouter()
@@ -9,15 +16,25 @@ def get_genres_hello():
     """function to check the route"""
     return HTMLResponse('<h1>Hola desde la ruta de genres</h1>')
 
+
 @genres_router.get('/genres', tags = ['genres'], status_code=200)
 def get_genres():
-    #funcion que trae todos los generos que esta en servicios
-    return JSONResponse(content={'message':'estos son los generos'})
+    db = Session()
+    result = GenresService(db).get_genres()
+    return JSONResponse(content=jsonable_encoder(result),status_code = 200)
+
+
+@genres_router.get('/genres_for_id', tags=['genres'], status_code=200)
+def get_genres_for_id(id:int):
+    db = Session()
+    result = GenresService(db).get_for_id(id)
+    return JSONResponse(content=jsonable_encoder(result),status_code = 200)
 
 @genres_router.post('/genres', tags=['genres'], status_code=201)
-def create_genres():
-    #llamar una funcion que va a estar en el servicios 
-    return JSONResponse(content={"message":"genre created successfully"})
+def create_genres(genres:Genres):
+    db = Session()
+    GenresService(db).create_genre(genres)
+    return JSONResponse(content={"message":"genre created successfully",'status_code':201}, status_code=201)
 
 #creamos un get que trae un solo genero por id 
 
